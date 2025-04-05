863dc0a75870af193871099388019e5298e332ca
dhanraj996 <luckydhana008@gmail.com>
2025-04-05 20:52:28 +0530

    terraform vpc module

---
+++ variables.tf
@@ -0,0 +1,77 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "common_tags" {
+    type= map
+    
+}
+
+variable "vpc_cidr" {
+  
+}
+
+variable "enable_dns_hostnames" {
+
+    default= true
+  
+}
+
+variable "vpc_tags"{
+
+    default = {
+
+    }
+  
+}
+
+variable "igw_tags"{
+    default = {}
+  
+}
+#Public subnet
+variable "public_subnet_cidrs"{
+    type = list
+
+    validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "please enter 2 valid public subnet CIDR"
+  }
+}
+
+variable "public_subnet_tags"{
+    default = {}
+}
+
+
+#private subnet
+variable "private_subnet_cidrs"{
+    type = list
+
+    validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "please enter 2 valid private subnet CIDR"
+  }
+}
+
+variable "private_subnet_tags"{
+    default = {}
+}
+
+#Database subnet
+variable "database_subnet_cidrs"{
+    type = list
+
+    validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "please enter 2 valid database subnet CIDR"
+  }
+}
+
+variable "database_subnet_tags"{
+    default = {}
+}
