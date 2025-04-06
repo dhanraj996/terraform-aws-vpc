2cb72f3795f2da89a8014b1ef72eba5ac5596dc0
dhanraj996 <luckydhana008@gmail.com>
2025-04-06 17:40:05 +0530

    terraform vpc

--- main.tf
+++ main.tf
@@ -149,5 +149,5 @@ resource "aws_route" "private" {
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id = aws_internet_gateway.example.id
 }
